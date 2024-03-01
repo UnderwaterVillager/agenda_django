f892f15157dc7bf40f9ebc62e93f73aabb844198
Vinicius-Jesus <vjbfvinicius@hotmail.com>
2024-03-01 09:29:07 -0300

    Adjusting pages, styles, and respective urls

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,10 @@
+from typing import Any
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
