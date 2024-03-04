940586135ce579d6e3931695c1f098b31416d87d
Vinicius-Jesus <vjbfvinicius@hotmail.com>
2024-03-04 16:46:19 -0300

    Add verbose to category, add fake data and prepare local_settings

--- contact/models.py
+++ contact/models.py
@@ -1,10 +1,15 @@
 from typing import Any
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural =  'Categories'   
+    
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -20,6 +25,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey (User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
