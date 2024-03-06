355b4ec3b545ce984f153c37a913dc98c8aee729
Vinicius-Jesus <vjbfvinicius@hotmail.com>
2024-03-06 15:35:16 -0300

    Implemented single contact

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,9 @@ from contact import views
 
 app_name = 'contact'
 
+
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
 
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from contact.models import Contact
 # Create your views here.
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.all().filter(show=True).order_by('-id')[0:10]
     context = {
         'contacts': contacts,
     }
 
     return render(request, 'contact/index.html', context)
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(request, 'contact/contact.html', context)
