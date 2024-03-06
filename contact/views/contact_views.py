f4370820421af7b05a10faf3a4aa5aeeb0866405
Vinicius-Jesus <vjbfvinicius@hotmail.com>
2024-03-06 17:08:22 -0300

    Filter and polish search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
+from django.db.models import Q
 
 from contact.models import Contact
 # Create your views here.
@@ -9,6 +10,23 @@ def index(request):
     contacts = Contact.objects.all().filter(show=True).order_by('-id')[0:10]
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(request, 'contact/index.html', context)
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) | Q(phone__icontains=search_value) | Q(email__icontains=search_value)).order_by('-id')
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
+        'search_value': search_value,
     }
 
     return render(request, 'contact/index.html', context)
@@ -16,9 +34,10 @@ def index(request):
 
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
-
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} -'
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(request, 'contact/contact.html', context)
