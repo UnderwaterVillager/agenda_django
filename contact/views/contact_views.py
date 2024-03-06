75173182c6951ae332fe045df55749f3b81a3962
Vinicius-Jesus <vjbfvinicius@hotmail.com>
2024-03-06 17:54:58 -0300

    Add pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 from contact.models import Contact
 # Create your views here.
 
 
 def index(request):
-    contacts = Contact.objects.all().filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.all().filter(show=True).order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
-        'site_title': 'Contatos - '
+        'page_obj': page_obj,
+        'site_title': 'Contacts - '
     }
 
     return render(request, 'contact/index.html', context)
@@ -18,14 +23,18 @@ def index(request):
 
 def search(request):
     search_value = request.GET.get('q', '').strip()
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) | Q(phone__icontains=search_value) | Q(email__icontains=search_value)).order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     if search_value == '':
         return redirect('contact:index')
 
-    contacts = Contact.objects.filter(show=True).filter(
-        Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) | Q(phone__icontains=search_value) | Q(email__icontains=search_value)).order_by('-id')
     context = {
-        'contacts': contacts,
-        'site_title': 'Contatos - ',
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
         'search_value': search_value,
     }
 
