1bf3878d22f9ac55e15770c975e4bdaca38dea3f
Vinicius-Jesus <vjbfvinicius@hotmail.com>
2024-03-12 15:02:43 -0300

    Add update and delete button for contact

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,21 +1,50 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from contact.create_contact_model import ContactForm
+from contact.models import Contact
 # Create your views here.
 
-
-
 def create(request):
+    form_action = reverse("contact:create")
     if request.method == "POST":
         form = ContactForm(request.POST)
         context = {
             'form': form,
+            'form_action': form_action
         }
         if form.is_valid():
-            form.save()
-            return redirect('contact:create')
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
         return render(request, 'contact/create.html', context)
-    
+    form = ContactForm()
     context = {
-            'form': ContactForm(),
+            'form': form,
+            'form_action': form_action
         }
     return render(request, 'contact/create.html', context)
+
+def update(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    form_action = reverse("contact:update", args=(contact_id,))
+    if request.method == "POST":
+        form = ContactForm(request.POST, instance=contact)
+        context = {
+            'form': form,
+            'form_action': form_action
+        }
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+        return render(request, 'contact/create.html', context)
+    form = ContactForm(instance=contact)
+    context = {
+            'form': form,
+            'form_action': form_action
+        }
+    return render(request, 'contact/create.html', context)
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    return render(request, )
