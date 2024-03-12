963e9f4ad4b334eaeaa360e3296e4eef7eef31d3
Vinicius-Jesus <vjbfvinicius@hotmail.com>
2024-03-12 16:32:29 -0300

    Add delete view

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -47,4 +47,14 @@ def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
-    return render(request, )
+
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+    return render(request, 'contact/contact.html', {
+        'contact': contact,
+        'confirmation': confirmation,
+        }
+    )
